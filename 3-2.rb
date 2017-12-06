de50291b3ba7c1f1f30e0f059d8cd0ee1e036dbc
Forrest Fleming <ffleming@gmail.com>
2017-12-05 21:52:43 -0800

    3 pt 2

--- 3-2.rb
+++ 3-2.rb
@@ -1,93 +1,113 @@
 require 'pry-byebug'
-target = 347991
-def layer_for(num)
-  base = 1
-  layer = 0
-  while base**2 < num
-    base += 2
-    layer += 1
+class Walker
+  attr_accessor :matrix, :size, :origin, :dir, :current, :target
+  def initialize(size: 1000, target: 347991)
+    @size = size
+    @target = target
+    @matrix = Array.new(size) { Array.new(size, 0) }
+    @origin = [size / 2, size / 2]
+    @matrix[size / 2][size / 2] = 1
+    @dir = nil
+    @current = nil
   end
-  layer
+
+  def walk!
+    @current = origin
+    @dir = next_dir!
+    loop do
+      go(dir)
+      puts "went #{dir}"
+      result = neighbor_sum
+      write(result)
+      puts "wrote #{result}"
+      break if result > target
+      if should_turn?
+        @dir = next_dir!
+      end
+    end
+  end
+
+  def status
+    ret = matrix.map do |row|
+      row.map(&:to_s).map {|el| el.center(5, " ")}.join(" ")
+    end.join("\n")
+    puts ret
+  end
+
+
+  def go(dir)
+    self.x += directions.fetch(dir)[1]
+    self.y += directions.fetch(dir)[0]
+  end
+
+  def x
+    current[1]
+  end
+
+  def y
+    current[0]
+  end
+
+  def x=(val)
+    current[1] = val
+  end
+
+  def y=(val)
+    current[0] = val
+  end
+
+  NEIGHBORS = [
+    [-1, -1],
+    [-1, 0],
+    [-1, 1],
+    [0, -1],
+    [0, 1],
+    [1, -1],
+    [1, 0],
+    [1, 1],
+  ]
+  def neighbor_sum
+    ns = NEIGHBORS.map do |delta_y, delta_x|
+      matrix[y + delta_y][x + delta_x]
+    end
+    ns.sum
+  end
+
+  def write(val)
+    matrix[y][x] = val
+  end
+
+  def should_turn?
+    look(peek_dir) == 0
+  end
+
+  def look(_dir)
+    delta_x = directions.fetch(_dir)[1]
+    delta_y = directions.fetch(_dir)[0]
+    matrix[y + delta_y][x + delta_x]
+  end
+
+
+  def peek_dir
+    arr = %i(right up left down)
+    i = arr.index(dir)
+    arr[(i + 1) % 4]
+  end
+
+  def next_dir!
+    @_next_dir ||= 0
+    ret = %i(right up left down)[@_next_dir % 4]
+    @_next_dir += 1
+    ret
+  end
+
+  def directions
+    @directions ||= {
+      right: [0, 1],
+      up:  [-1, 0],
+      left:  [0, -1],
+      down:    [1, 0],
+    }
+  end
 end
-
-def side_for_layer(layer)
-  layer * 2 + 1
-end
-
-def bottom_right_of_layer(layer)
-  side_for_layer(layer)**2
-end
-
-def bottom_left_of_layer(layer)
-  bottom_right_of_layer(layer) - (side_for_layer(layer) - 1)
-end
-
-def top_left_of_layer(layer)
-  bottom_left_of_layer(layer) - (side_for_layer(layer) - 1)
-end
-
-def top_right_of_layer(layer)
-  top_left_of_layer(layer) - (side_for_layer(layer) - 1)
-end
-
-def min_for_layer(layer)
-  bottom_right_of_layer(layer + 1) - 1
-end
-
-def is_top?(num)
-  layer = layer_for(num)
-  (top_right_of_layer(layer)..top_left_of_layer(layer)).include?(num)
-end
-
-def is_right?(num)
-  layer = layer_for(num)
-  (min_for_layer(layer)..top_right_of_layer(layer)).include?(num)
-end
-
-def is_bottom?(num)
-  layer = layer_for(num)
-  (bottom_left_of_layer(layer)..bottom_right_of_layer(layer)).include?(num)
-end
-
-def is_left?(num)
-  layer = layer_for(num)
-  (top_left_of_layer(layer)..bottom_left_of_layer(layer)).include?(num)
-end
-
-def is_corner?(num)
-  layer = layer_for(num)
-  num == top_left_of_layer(layer) ||
-    num == top_right_of_layer(layer) ||
-    num == bottom_left_of_layer(layer) ||
-    num == bottom_right_of_layer(layer)
-end
-
-
-def distance_for(num)
-  layer = layer_for(num)
-  # if is_corner?(num)
-  #   layer * 2
-  # end
-  x_offset = if is_left?(num) || is_right?(num)
-               layer
-             elsif is_top?(num)
-               mid = (top_right_of_layer(layer) + top_left_of_layer(layer)) / 2
-               (num - mid).abs
-             elsif is_bottom?(num)
-               mid = (bottom_right_of_layer(layer) + bottom_left_of_layer(layer)) / 2
-               (num - mid).abs
-             end
-  y_offset = if is_top?(num) || is_bottom?(num)
-               layer
-             elsif is_left?(num)
-               mid = (bottom_left_of_layer(layer) + top_left_of_layer(layer)) / 2
-               (num - mid).abs
-             elsif is_right?(num)
-               mid = (min_of_layer(layer) + top_right_of_layer(layer) - 1 ) / 2
-               (num - mid).abs
-             end
-
-  x_offset + y_offset
-end
-
-Pry.start
+Walker.new.walk!
